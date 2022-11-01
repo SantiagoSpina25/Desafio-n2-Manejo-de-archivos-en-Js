e91bf2eccf5bddac38ba97095235aca8ef46db02
Santiago Spina <santiagospina025@gmail.com>
2022-11-01 17:13:03 +0100

    Entrega

---
+++ index.js
@@ -0,0 +1,23 @@
+const Container = require("./container")
+const products = new Container("./products.json")
+
+async function ejecutar(){
+
+    
+    
+    productoNuevo1={
+        name: "Bruja Escarlata - Doctor Strange en el Multiverso de la Locura",
+        price: 12.70,
+        thumbnail: "https://www.popking.es/wp-content/uploads/2022/03/funko-pop-bruja-escarlata-doctor-strange-en-el-multiverso-de-la-locura.jpg",
+        category: "Marvel"
+    }
+
+    productoNuevo2={
+        name: "Michael Jordan (All Star 1988)",
+        price: 12.90,
+        thumbnail: "https://www.popking.es/wp-content/uploads/2022/02/funko-pop-michael-jordan-all-star-1988-nba.jpg",
+        category: "NBA"
+    }
+    
+}
+ejecutar()
